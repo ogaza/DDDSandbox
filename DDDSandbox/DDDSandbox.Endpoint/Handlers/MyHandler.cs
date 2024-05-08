90226ece2d2dba72bccf1b18e8d09e7df212ec6b
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-05-08 10:00:26 +0200

    remove publishing the OrderCreated event from the example DDDSandbox.Endpoint project

--- DDDSandbox/DDDSandbox.Endpoint/Handlers/MyHandler.cs
+++ DDDSandbox/DDDSandbox.Endpoint/Handlers/MyHandler.cs
@@ -1,5 +1,4 @@
 ﻿using DDDSandbox.Messages;
-using DDDSandbox.Messages.Events;
 using NServiceBus;
 
 namespace DDDSandbox.Endpoint.Handlers
@@ -7,41 +6,9 @@ namespace DDDSandbox.Endpoint.Handlers
   public class MyHandler :
     IHandleMessages<MyMessage>
   {
-    //static ILog log = LogManager.GetLogger<MyHandler>();
-
-    public async Task Handle(MyMessage message, IMessageHandlerContext context)
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
-      var id = DataBase.SaveOrder(message.Name);
-
-      // this handler sends new version of the order created event
-      // since OrderCreated_V2 inherits from OrderCreated
-      // all the OrderCreated handlers will also handle the 
-      // OrderCreated_V2 event
-      var orderCreatedEvent = new OrderCreated_V2 { OrderId = $"{id}", AddressId = "SomeAddressId" };
-      await context.Publish(orderCreatedEvent);
-      
-      
-      //await context.Reply(message);
-      //log.Info("Message received at endpoint");
-      //return Task.CompletedTask;
-    }
-  }
-
-  public static class DataBase 
-  {
-    private static int Id = 0;
-
-    private static readonly List<string> orderNames = new();
-
-    internal static int SaveOrder(string? name)
-    {
-      if (name == null) 
-      {
-        return 0;
-      }
-      orderNames.Add(name);
-
-      return ++Id;
+      return Task.CompletedTask;
     }
   }
 }
--- DDDSandbox/DDDSandbox.Endpoint/Program.cs
+++ DDDSandbox/DDDSandbox.Endpoint/Program.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NServiceBus;
 
-Console.WriteLine("Hello, World!");
-
 Console.Title = "DDDSandbox.Endpoint";
 var endpointConfiguration = new EndpointConfiguration("DDDSandbox.Endpoint");
-//endpointConfiguration.UsePersistence<LearningPersistence>();
 endpointConfiguration.UseTransport(new LearningTransport());
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
@@ -13,7 +10,6 @@ var endpointInstance = await Endpoint.Start(endpointConfiguration);
 Console.WriteLine("Press any key to exit");
 Console.ReadKey();
 await endpointInstance.Stop();
-
 
 // probably instead of running as a console app
 // wouold be better to create a nservicebus host
