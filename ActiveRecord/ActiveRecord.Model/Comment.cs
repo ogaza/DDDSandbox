ca81fd73366388f3a7221b1198d8d811595f6e87
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-06-11 13:30:58 +0200

    ActiveRecord: ActiveRecord.Model project with base classes definitions

---
+++ ActiveRecord/ActiveRecord.Model/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace ActiveRecord.Model
+{
+  public class Comment
+  {
+    public int Id { get; set; }
+
+    public Post? Post { get; set; }
+
+    public string? Text { get; set; }
+
+    public string? Author { get; set; }
+
+    public DateTime? Created { get; set; }
+  }
+}
---
+++ ActiveRecord/ActiveRecord.Model/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace ActiveRecord.Model
+{
+  public class Post
+  {
+    public int Id { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? Text { get; set; }
+
+    public IList<Comment>? Comments { get; set; }
+
+    public DateTime? Created { get; set; }
+  }
+}
