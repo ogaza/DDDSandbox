05194fc9ebbf198741ea2933a64bd852102a6bd4
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-06-11 14:10:40 +0200

    ActiveRecord: connect web api with the model. return some mocked Post list from the BlogController

--- ActiveRecord/ActiveRecord.Model/Post.cs
+++ ActiveRecord/ActiveRecord.Model/Post.cs
@@ -11,5 +11,16 @@
     public IList<Comment>? Comments { get; set; }
 
     public DateTime? Created { get; set; }
+
+    public static IEnumerable<Post> GetAll()
+    {
+      var list = new List<Post>()
+      {
+        new() { Id = 1},
+        new() { Id = 2}
+      };
+
+      return list;
+    }
   }
 }
--- ActiveRecord/ActiveRecord.Web/Controllers/BlogController.cs
+++ ActiveRecord/ActiveRecord.Web/Controllers/BlogController.cs
@@ -1,18 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ActiveRecord.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ActiveRecord.Web.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("[controller]")]
   [ApiController]
   public class BlogController : ControllerBase
   {
     // GET: api/blog/posts
     [HttpGet("Posts", Name = "GetPosts")]
-    public async Task<IEnumerable<string>> GetPosts()
+    public async Task<IEnumerable<Post>> GetPosts()
     {
-      var list = new []{ "1" };
-
-      return await Task.FromResult(list);
+      return await Task.FromResult(Post.GetAll());
     }
   }
 }
