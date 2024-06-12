eb0c4972226a0a4ab4fa6b9687fb168985cf588a
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-06-12 10:43:11 +0200

    ActiveRecord: retrieving the Blog db connection string from the app's configuration during the app's start

---
+++ ActiveRecord/ActiveRecord.Model/DBConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace ActiveRecord.Model
+{
+  public static class DBConfiguration
+  {
+    private static bool _initialised = false;
+    private static string? _connectionString { get; set; }
+
+    public static void SetConnectionString(string connectionString)
+    {
+      if (_initialised) return;
+
+      _connectionString = connectionString;
+      _initialised = true;
+    }
+
+    public static string? ConnectionString => 
+      _connectionString;
+  }
+}
--- ActiveRecord/ActiveRecord.Web/Program.cs
+++ ActiveRecord/ActiveRecord.Web/Program.cs
@@ -1,8 +1,14 @@
+using ActiveRecord.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+string blogDBConnectionString = 
+  builder.Configuration.GetConnectionString("BlogDB");
+DBConfiguration.SetConnectionString(blogDBConnectionString);
 
 app.MapControllers();
 app.UseStaticFiles();
