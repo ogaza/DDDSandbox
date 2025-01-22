73420033c6143ab3dac43d16de080ff2b13d5098
Olaf Gaża <olaf.gaza.fp@gmail.com>
2025-01-22 15:37:47 +0100

    DesignPrinciples: Interface Segregation Principle example

---
+++ DesignPrinciples/ISP.Model/model.cs
@@ -0,0 +1,40 @@
+﻿namespace ISP.Model
+{
+  public interface IProduct
+  {
+    decimal Price { get; set; }
+    decimal WeightInKg { get; set; }
+    int Stock {  get; set; }
+  }
+
+  public interface IMovie
+  {
+    int Certification { get; set; }
+    int RunningTime { get; set; }
+  }
+
+  public class DVD : IProduct, IMovie
+  {
+    public decimal Price { get; set;}
+    public decimal WeightInKg { get; set; }
+    public int Stock { get; set; }
+    public int Certification { get; set; }
+    public int RunningTime { get; set; }
+  }
+
+  public class BlueRay : IProduct, IMovie
+  {
+    public decimal Price { get; set; }
+    public decimal WeightInKg { get; set; }
+    public int Stock { get; set; }
+    public int Certification { get; set; }
+    public int RunningTime { get; set; }
+  }
+  
+  public class TShirt : IProduct
+  {
+    public decimal Price { get; set; }
+    public decimal WeightInKg { get; set; }
+    public int Stock { get; set; }
+  }
+}
